14ba508eee908241e33e79455667105dca56b220
A .M <ahmedzzabdalla0@gmail.com>
2025-08-11 14:31:08 +0300

    .

---
+++ app/(routes)/book/[...path]/page.js
@@ -0,0 +1,96 @@
+import Sidebar from "@/components/book/sidebar";
+import { feminineNumericWord, numericWord } from "@/static/numical";
+import { Lessons } from "@/static/lessons";
+import { Stages } from "@/static/stages";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const pathes = [];
+  Stages.forEach((stage) => {
+    stage.years.forEach((year) => {
+      year.units.forEach((unit) => {
+        pathes.push([stage.id, year.id, unit.id]);
+      });
+    });
+  });
+
+  return pathes;
+}
+
+export async function generateMetadata({ params }) {
+  const { path } = await params;
+  const [stage_id, year_id, unit_id] = path;
+  const lessons = Lessons[stage_id]?.[year_id]?.[unit_id];
+  if (!stage_id || !year_id || !unit_id || !lessons) {
+    return notFound();
+  }
+  const stage = Stages.find((stage) => stage.id === stage_id);
+  const year = stage?.years.find((year) => year.id === year_id);
+  const unit = year?.units.find((unit) => unit.id === unit_id);
+  const unit_index = year?.units.findIndex((unit) => unit.id === unit_id);
+  const year_label = year?.label;
+  const unit_label = unit?.label;
+
+  return {
+    title: `${unit_label} - ${year_label}`,
+    description: `تعرّف على الوحدة ${feminineNumericWord[unit_index]}:${unit_label} في ${year_label}`,
+    authors: [
+      {
+        name: "الباحثة جيهان زايد",
+        title: "قائدة فريق تطوير منهج العلوم في ضوء مدخل STEAM",
+      },
+    ],
+  };
+}
+
+export default async function LessonsPage({ params }) {
+  const { path } = await params;
+  const [stage_id, year_id, unit_id] = path;
+  const lessons = Lessons[stage_id]?.[year_id]?.[unit_id];
+  if (!stage_id || !year_id || !unit_id || !lessons) {
+    return notFound();
+  }
+  const stage = Stages.find((stage) => stage.id === stage_id);
+  const year = stage?.years.find((year) => year.id === year_id);
+  const unit = year?.units.find((unit) => unit.id === unit_id);
+  const unit_index = year?.units.findIndex((unit) => unit.id === unit_id);
+  const year_label = year?.label;
+  const unit_label = unit?.label;
+
+  return (
+    <div className="flex flex-col sm:flex-row absolute inset-0 gap-x-4">
+      {/* Sidebar */}
+      <Sidebar />
+      <div
+        className="bg-neutral-1000/[97%] [background-blend-mode:lighten] shadow-md m-4 mb-0 sm:mb-4 mr-0 rounded-m w-full p-7 overflow-y-scroll [-webkit-overflow-scrolling:touch] relative bg-fixed bg-center bg-cover"
+        style={{ backgroundImage: `url(/bg-light.svg)` }}
+      >
+        <div className="shadow-md bg-primary text-neutral-200 w-fit rounded-md mb-4 relative overflow-hidden">
+          <span className="block px-3 py-2 font-content-bold text-neutral-200">
+            {year_label}
+          </span>
+        </div>
+        <h1 className="font-highlight-bold font-black text-primary mb-5">
+          <span className="mx-2">{">"}</span>
+          <span className="underline underline-offset-4">{`الوحدة ${feminineNumericWord[unit_index]}: ${unit_label}`}</span>
+        </h1>
+        <div className="grid [grid-template-columns:repeat(auto-fit,minmax(250px,1fr))] gap-7 h-fit relative">
+          {lessons.map((lesson, i) => (
+            <div
+              key={i}
+              className="bg-white/60 bg-clip-padding backdrop-filter backdrop-blur-[5px] border border-primary/20
+ rounded-m p-5 clickable"
+            >
+              <h6 className="font-heading-6 font-black text-primary-400 pointer-events-auto">
+                {`الدرس ${numericWord[i]}`}
+              </h6>
+              <p className="font-content-bold text-primary mt-2">
+                {lesson.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/static/lessons.js
@@ -0,0 +1,226 @@
+export const Lessons = {
+  intermediate: {
+    first_year: {
+      life_sciences: [
+        {
+          id: "cell_and_exploration_of_life",
+          label: "الخلية واستكشاف الحياة.",
+        },
+        {
+          id: "transport_processes_in_the_cell",
+          label: "عمليات النقل في الخلية.",
+        },
+        {
+          id: "levels_of_organization_in_living_organisms",
+          label: "مستويات التنظيم في الكائنات الحية.",
+        },
+        {
+          id: "enrichment_and_expansion_stem_cells",
+          label: "الاثراء والتوسع: الخلايا الجذعية",
+        },
+        { id: "genetic_material", label: "المادة الوراثية." },
+        { id: "reproduction", label: "التكاثر." },
+        { id: "inheritance", label: "الوراثة." },
+        {
+          id: "enrichment_and_expansion_genetic_fingerprint",
+          label: "الاثراء والتوسع: البصمة الوراثية.",
+        },
+      ],
+      earth_sciences: [
+        { id: "ecosystems", label: "الأنظمة البيئية" },
+        {
+          id: "biological_communities_and_their_interrelationships",
+          label: "الجماعات الحيوية وعلاقتها المتبادلة.",
+        },
+        {
+          id: "enrichment_and_expansion_ecosystem_health_nationalism",
+          label: "الاثراء والتوسع: صحة الأنظمة البيئية الوطنية.",
+        },
+        {
+          id: "relative_and_absolute_age_dating",
+          label: "التأريخ بالعمر النسبي والمطلق.",
+        },
+        { id: "rock_record", label: "السجل الصخري." },
+        {
+          id: "enrichment_and_expansion_geology_and_technology",
+          label: "الاثراء والتوسع: علم الأرض والتقنية (اكتشاف أنسجة الديناصور)",
+        },
+      ],
+      physical_sciences: [
+        { id: "structure_of_matter", label: "تركيب المادة." },
+        { id: "periodic_table", label: "الجدول الدوري." },
+        {
+          id: "enrichment_and_expansion_nuclear_reactors",
+          label: "الاثراء والتوسع: المفاعلات النووية",
+        },
+        { id: "chemical_bonds", label: "الروابط الكيميائية" },
+        { id: "chemical_reactions", label: "التفاعلات الكيميائية" },
+        {
+          id: "enrichment_and_expansion_nanomaterials",
+          label: "الاثراء والتوسع: المواد النانوية",
+        },
+      ],
+      design_and_manufacturing: [
+        { id: "industrial_drawing", label: "الرسم الصناعي." },
+        { id: "projection", label: "الأسقاط." },
+        { id: "perspective", label: "المنظور" },
+        { id: "introduction_to_robotics", label: "مقدمة في علم الروبوتات" },
+        { id: "educational_robot", label: "الروبوت التعليمي" },
+        { id: "robot_turning", label: "انعطاف الروبوت" },
+      ],
+    },
+    second_year: {
+      life_sciences: [
+        { id: "animal_behavior", label: "سلوك الحيوان" },
+        { id: "adaptation_and_extinction", label: "التكيف والانقراض" },
+        {
+          id: "enrichment_and_expansion_technology_in_identifying_extinct_species",
+          label:
+            "الإثراء والتوسع: كيف تُسهم التكنولوجيا في تعرّف الكائنات الحية المنقرضة؟",
+        },
+        { id: "evolution_of_living_organisms", label: "تطور الكائنات الحية" },
+        {
+          id: "mechanisms_of_evolution_in_living_organisms",
+          label: "آليات تطور الكائنات الحية",
+        },
+        {
+          id: "enrichment_and_expansion_artificial_selection",
+          label: "الإثراء والتوسع: الانتخاب الصناعي",
+        },
+      ],
+
+      earth_sciences: [
+        { id: "atmosphere", label: "الغلاف الجوي" },
+        { id: "air_masses", label: "الكتل الهوائية" },
+        { id: "atmospheric_impact", label: "تأثيرات الغلاف الجوي" },
+        {
+          id: "enrichment_and_expansion_aerogel",
+          label: "الإثراء والتوسع: الإيروجل (الهلام الهوائي)",
+        },
+        { id: "ecosystem_and_earth", label: "النظام البيئي والأرضي" },
+        {
+          id: "matter_and_energy_in_ecosystems",
+          label: "المادة والطاقة في لأنظمة البيئية",
+        },
+        {
+          id: "enrichment_and_expansion_carbon_footprint",
+          label: "الاثراء والتوسع: البصمة الكربونية",
+        },
+      ],
+      physical_sciences: [
+        { id: "work_and_energy", label: "الشغل والطاقة" },
+        { id: "simple_machines", label: "الآلات البسيطة" },
+        {
+          id: "enrichment_and_expansion_intelligent_transportation",
+          label: "الإثراء والتوسع: النقل الذكي",
+        },
+        { id: "water", label: "الماء" },
+        { id: "solubility", label: "الذوبانية" },
+        {
+          id: "enrichment_and_expansion_saline_solutions",
+          label: "الإثراء والتوسع: المحاليل المالحة",
+        },
+        { id: "static_electricity", label: "الكهرباء الساكنة" },
+        { id: "current_electricity", label: "الكهرباء المتحركة" },
+        {
+          id: "enrichment_and_expansion_self_driving_cars",
+          label: "الإثراء والتوسع: السيارات ذاتية القيادة",
+        },
+        {
+          id: "acidic_and_basic_solutions",
+          label: "المحاليل الحامضية والقاعدية",
+        },
+        {
+          id: "indicators_of_acidic_and_basic_solutions",
+          label: "كواشف المحاليل الحامضية والقاعدية",
+        },
+        {
+          id: "enrichment_and_expansion_saline_solutions_2",
+          label: "الإثراء والتوسع: المحاليل المالحة",
+        },
+      ],
+      design_and_manufacturing: [
+        { id: "robot_sensors", label: "مستشعرات الروبوت" },
+        {
+          id: "robot_programming_for_decision_making",
+          label: "برمجة الروبوتات لاتخاذ قرارات",
+        },
+        { id: "map_creation", label: "إنشاء الخرائط" },
+        { id: "virtual_robots", label: "الروبوتات الافتراضية" },
+        { id: "coordinates_in_programming", label: "الإحداثيات في البرمجة" },
+        { id: "automatic_movement", label: "الحركة التلقائية" },
+      ],
+    },
+    third_year: {
+      life_sciences: [
+        {
+          id: "regulation_and_organization_of_the_human_body",
+          label: "ضبط وتنظيم جسم الانسان.",
+        },
+        { id: "integration_of_human_systems", label: "تكامل أجهزة الانسان." },
+        {
+          id: "enrichment_and_expansion_parkinsons_disease",
+          label: "الإثراء والتوسع: داء باركنسون",
+        },
+        { id: "immunity", label: "المناعة" },
+        {
+          id: "sexual_reproduction_in_humans",
+          label: "التكاثر الجنسي في الانسان",
+        },
+        {
+          id: "enrichment_and_expansion_parkinsons_disease_2",
+          label: "الإثراء والتوسع: داء باركنسون",
+        },
+      ],
+
+      earth_sciences: [
+        { id: "tectonic_plates", label: "الصفائح التكتونية" },
+        { id: "earthquakes", label: "الزلازل" },
+        { id: "volcanoes", label: "البراكين" },
+        {
+          id: "enrichment_and_expansion_artificial_intelligence_application",
+          label: "الإثراء والتوسع: تطبيق الذكاء الاصطناعي",
+        },
+        { id: "space_exploration", label: "استكشاف الفضاء" },
+        { id: "current_space_missions", label: "البعثات الفضائية الحالية" },
+        {
+          id: "enrichment_and_expansion_global_positioning_system",
+          label: "الاثراء والتوسع: نظام تحديد الموقع العالمي",
+        },
+      ],
+      physical_sciences: [
+        { id: "motion", label: "الحركة" },
+        { id: "force", label: "القوة" },
+        {
+          id: "enrichment_and_expansion_air_bags",
+          label: "الإثراء والتوسع: الوسائد الهوائية",
+        },
+        { id: "waves", label: "الموجات" },
+        { id: "sound_waves", label: "الموجات الصوتية" },
+        {
+          id: "enrichment_and_expansion_doppler_effect",
+          label: "الإثراء والتوسع: ظاهرة دوبلر",
+        },
+        { id: "magnetism", label: "المغناطيسية" },
+        { id: "electromagnetism", label: "الكهرومغناطيسية" },
+        {
+          id: "enrichment_and_expansion_magnetic_levitation_train",
+          label: "الإثراء والتوسع: قطار الرفع المغناطيسي",
+        },
+        { id: "visible_light", label: "الضوء المرئي" },
+        { id: "mirrors", label: "المرايا" },
+        {
+          id: "enrichment_and_expansion_magnetic_levitation_train_2",
+          label: "الإثراء والتوسع: قطار الرفع المغناطيسي",
+        },
+      ],
+      design_and_manufacturing: [
+        { id: "robot_control", label: "التحكم في الروبوت" },
+        { id: "synthetic_programming", label: "البرمجة التركيبية" },
+        { id: "robot_sensors", label: "مستشعرات الروبوت" },
+        { id: "eye_sensors", label: "مستشعرات العين" },
+        { id: "collision_sensors", label: "مستشعرات الاصطدام" },
+      ],
+    },
+  },
+};
