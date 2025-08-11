3beea4bd2f94e78bb8693b35d05f961e61f5a5c6
A .M <ahmedzzabdalla0@gmail.com>
2025-08-11 17:10:49 +0300

    .

--- app/(routes)/book/layout.js
+++ app/(routes)/book/layout.js
@@ -8,11 +8,14 @@ export default function RootLayout({ children }) {
       <div className="flex flex-col sm:flex-row absolute inset-0 gap-x-4">
         <Sidebar />
         <div className="flex flex-col gap-y-4 w-full p-4 pb-0 sm:pb-4 sm:pr-0">
-          <div className="bg-neutral-1000 border border-primary-400/5 rounded-md py-4 px-7 overflow-y-scroll [-webkit-overflow-scrolling:touch] relative bg-fixed bg-top-right bg-cover">
+          <div
+            className="bg-neutral-1000 border border-primary-400/5 rounded-md py-4 px-7 overflow-y-scroll [-webkit-overflow-scrolling:touch] relative bg-fixed [background-position:0_0] bg-cover"
+            style={{ backgroundImage: `url(/bg-light.png)` }}
+          >
             <Breadcrumbs />
           </div>
           <div
-            className="bg-neutral-1000 h-full border border-primary-400/5 rounded-md p-7 overflow-y-scroll [-webkit-overflow-scrolling:touch] relative bg-fixed bg-top-right bg-cover"
+            className="bg-neutral-1000 h-full border border-primary-400/5 rounded-md p-7 overflow-y-scroll [-webkit-overflow-scrolling:touch] relative bg-fixed [background-position:0_-16px] bg-cover"
             style={{ backgroundImage: `url(/bg-light.png)` }}
           >
             {children}
