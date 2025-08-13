9123c81d5ed64db9519c626a272fb9ac4b9d4263
A .M <ahmedzzabdalla0@gmail.com>
2025-08-13 13:55:56 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+
+  async redirects() {
+    return process.env.SHOW_MAINTENANCE === "1"
+      ? [
+          {
+            source: "/((?!coming-soon).*)",
+            destination: "/coming-soon",
+            permanent: false,
+          },
+        ]
+      : [
+          {
+            source: "/coming-soon",
+            destination: "/",
+            permanent: false,
+          },
+        ];
   },
 };
 
